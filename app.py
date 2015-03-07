d5e315dcb53e22ac834cbae03facd11474ea577f
Thomas <thomaswhyyou@gmail.com>
2015-03-07 03:04:23 +0000

    Putting yolo on the screen.

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 # Let's build the simplest web app possible.
+
+if __name__ == "__main__":
+    config = Configurator()
+    config.add_route('hello', '/')
+    config.add_view(lambda request: Response('Yolo!'), route_name='hello')
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 5000, app)
+    server.serve_forever()
