7f4ec6e62c86e95ad3225bd438754de98057ed68
Thomas <thomaswhyyou@gmail.com>
2015-03-07 03:05:57 +0000

    Added some notes about wsgi

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-# Let's build the simplest web app possible.
+"""
+Objectives:
+Get the simplest Python web app running i.e. put pixels on your browser.
+
+WSGI:
+Python standard library that defines how Python web applications plug into standard servers,
+handling incoming requests and return responses. e.g. Ruby's Rack webserver interface
+"""
