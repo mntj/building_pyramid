f527502ccc244eca6afdf0e2662a5f287b76791d
Thomas <thomaswhyyou@gmail.com>
2015-03-07 05:11:26 +0000

    Starting point for step-5

--- app.py
+++ app.py
@@ -2,19 +2,16 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-from views import landing
+import views
+
 
 if __name__ == "__main__":
     config = Configurator()
 
     config.add_route('hello', '/')
-    config.add_view(landing.hello_world, route_name='hello')
-
     config.add_route('about', '/about')
-    config.add_view(landing.about_me, route_name='about')
-
     config.add_route('contact_page', '/contact')
-    config.add_view(landing.contact_me, route_name='contact_page')
+    config.scan(views)
 
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 5000, app)
--- views/landing.py
+++ views/landing.py
@@ -1,12 +1,17 @@
 from pyramid.response import Response
+from pyramid.view import view_config
 
-
+@view_config(route_name='hello')
 def hello_world(request):
     return Response('<body><h1>Yolo!</h1></body>')
 
+
+@view_config(route_name='about')
 def about_me(request):
     return Response('<body><h1>I am all about that bass.</h1></body>')
 
+
+@view_config(route_name='contact_page')
 def contact_me(request):
     return Response(
         """
