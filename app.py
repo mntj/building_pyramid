8a5bcb2abd57cdb1b09c3ec1dd4921888b523ed2
Thomas <thomaswhyyou@gmail.com>
2015-03-07 03:43:34 +0000

    Starting point for step-2

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-# Let's build the simplest web app possible.
+def hello_world(request):
+    return Response('<body><h1>Yolo!</h1></body>')
 
 if __name__ == "__main__":
     config = Configurator()
     config.add_route('hello', '/')
-    config.add_view(lambda request: Response('Yolo!'), route_name='hello')
+    config.add_view(hello_world, route_name='hello')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 5000, app)
     server.serve_forever()
